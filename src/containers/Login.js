f7fd3d9aa9397cb6348e871816b2a5e1945bf9e4
Ryan Yogan <ryanyogan@gmail.com>
2018-08-20 12:07:36 -0700

    added in login route/container

---
+++ src/containers/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import './Login.css';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  validateForm = () =>
+    this.state.email.length > 0 && this.state.password.length > 0;
+
+  handleChange = event =>
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
