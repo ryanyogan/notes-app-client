a028adbadcf7a77bdab1ab56852b7c08e77cd86b
Ryan Yogan <ryanyogan@gmail.com>
2018-08-20 11:37:31 -0700

    added in AWS Amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'notes-app-uploaded-assets',
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://pwxaatjwag.execute-api.us-east-1.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_ELWMdbcEn',
+    APP_CLIENT_ID: 'fah7ve0adacufr6qv6b6ivp03',
+    IDENTITY_POOL_ID: 'us-east-1:a3c49e45-161e-4e40-9cda-3b729469637e',
+  },
+};
